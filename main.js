5d3c38fda0dcfd79f0611ea03c19b1c7405cc271
Katsushika-h <chromamonster@gmail.com>
2025-09-08 16:33:48 +0800

    cowsay moo, tuna!

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const name = "tuna";
+import { moo } from './moo.js';
+import say from 'cowsay';
+
+
 
 console.log("Hello, World!");
-console.log(`moo, ${name}!`);
+console.log(say.say({
+    text: moo(name), 
+    e: "o o", 
+    }
+));
