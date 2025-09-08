e42fa94c6239976e9108960cdbf4140c18f041ee
Katsushika-h <chromamonster@gmail.com>
2025-09-08 16:58:43 +0800

    vite

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const name = "tuna";
 import { moo } from './moo.js';
-import say from 'cowsay';
+import {say} from 'cowsay';
 
 
 
 console.log("Hello, World!");
-console.log(say.say({
+console.log(say({
     text: moo(name), 
     e: "o o", 
     }
